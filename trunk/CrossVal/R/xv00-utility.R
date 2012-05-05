374c4da7abd067182a0dd65bea5685002fecdca7
plroebuck <plroebuck@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-05-05 13:24:26 +0000

    Reformatting.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/classpred@32 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/CrossVal/R/xv00-utility.R
+++ trunk/CrossVal/R/xv00-utility.R
@@ -1,14 +1,21 @@
-# split a dataset into training and testing sets, keeping a designated
-# factor balanced between the two sets.
+###
+### UTILITY.R
+###
+
+
+##-----------------------------------------------------------------------------
+## Split a dataset into training and testing sets, keeping a designated
+## factor balanced between the two sets.
+##
+## fac is the factor with respect to which the pieces should be balanced
+## size is a number between 0 and 1, the fraction to be used for training
 balancedSplit <- function(fac, size) {
-  # fac is the factor with respect to which the pieces should be balanced
-  # size is a number between 0 and 1, the fraction to be used for training
-  trainer <- rep(FALSE, length(fac))
-  for (lev in levels(fac)) {
-    N <- sum(fac==lev)
-    wanted <- max(1, trunc(N*size))
-    trainer[fac==lev][sample(N, wanted)] <- TRUE
-  }
-  trainer
+    trainer <- rep(FALSE, length(fac))
+    for (lev in levels(fac)) {
+        N <- sum(fac==lev)
+        wanted <- max(1, trunc(N*size))
+        trainer[fac==lev][sample(N, wanted)] <- TRUE
+    }
+    trainer
 }
 
