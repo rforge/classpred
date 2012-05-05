f4660d7abd9ac4a45aa8c4daab8efb2fb5e0f4a2
plroebuck <plroebuck@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-05-05 02:26:55 +0000

    Added Model class back in individual file.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/classpred@31 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ trunk/CrossVal/R/xv01-0-Model.R
@@ -0,0 +1,28 @@
+###
+### MODELER.R
+###
+
+##=============================================================================
+setClass("Modeler",
+         representation(learn="function",
+                        predict="function",
+                        params="list"))
+
+##-----------------------------------------------------------------------------
+## Generates a Modeler object
+Modeler <- function(learn, predict, ...) {
+    new("Modeler",
+        learn=learn,
+        predict=predict,
+        params=list(...))
+}
+
+
+##-----------------------------------------------------------------------------
+learn <- function(model, data, status) {
+    model@learn(data,
+                status,
+                model@params,
+                model@predict)
+}
+
